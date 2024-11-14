8481f2d849273e0909db977b6c64699be1a24693
tungnk123 <doanthanhtungnk123@gmail.com>
2024-11-14 21:21:54 +0700

    Initial commit

---
+++ stream_processing_demo/src/main/java/com/example/stream_processing_demo/StreamProcessingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.stream_processing_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamProcessingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamProcessingDemoApplication.class, args);
+	}
+
+}
---
+++ stream_processing_demo/src/test/java/com/example/stream_processing_demo/StreamProcessingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stream_processing_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamProcessingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
